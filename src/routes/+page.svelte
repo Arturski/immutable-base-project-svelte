0c6193126b2dfa2b0795e4a5203a38a360416f7c
Arthur <Arturski@users.noreply.github.com>
2023-08-29 12:04:25 +0200

    moment of truth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,120 +1,125 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { login } from "../auth";
-  import { JsonView } from '@zerodevx/svelte-json-view'
+	import { onMount } from 'svelte';
+	import { login } from '../auth';
+	import { JsonView } from '@zerodevx/svelte-json-view';
 
-  let jsonData: string | null = null;
-  let showStatus: boolean = false;
+	let jsonData: string | null = null;
+	let showStatus: boolean = false;
 
-  onMount(() => {
-    const keyPrefix = "oidc.user:https://auth.immutable.com";
-    if (typeof sessionStorage !== "undefined") {
-      for (let i = 0; i < sessionStorage.length; i++) {
-        const key = sessionStorage.key(i);
-        if (key && key.startsWith(keyPrefix)) {
-          const sessionData = sessionStorage.getItem(key);
-          if (sessionData) {
-            try {
-              jsonData = JSON.parse(sessionData);
-            } catch (error) {
-              console.error("Error parsing session data:", error);
-            }
-          }
-          break;
-        }
-      }
-    }
-  });
+	onMount(() => {
+		const keyPrefix = 'oidc.user:https://auth.immutable.com';
+		if (typeof sessionStorage !== 'undefined') {
+			for (let i = 0; i < sessionStorage.length; i++) {
+				const key = sessionStorage.key(i);
+				if (key && key.startsWith(keyPrefix)) {
+					const sessionData = sessionStorage.getItem(key);
+					if (sessionData) {
+						try {
+							jsonData = JSON.parse(sessionData);
+						} catch (error) {
+							console.error('Error parsing session data:', error);
+						}
+					}
+					break;
+				}
+			}
+		}
+	});
 
-  async function copyJsonToClipboard() {
-    if (jsonData) {
-      try {
-        await navigator.clipboard.writeText(JSON.stringify(jsonData, null, 2));
-        showStatus = true;
-      } catch (error) {
-        console.error("Error copying to clipboard:", error);
-      }
-    }
-  }
+	async function copyJsonToClipboard() {
+		if (jsonData) {
+			try {
+				await navigator.clipboard.writeText(JSON.stringify(jsonData, null, 2));
+				showStatus = true;
+			} catch (error) {
+				console.error('Error copying to clipboard:', error);
+			}
+		}
+	}
 </script>
 
 <div class="container dark-theme">
-  <h1 class="title">Base Template with Vite + SvelteKit + Immutable SDK + polyfills</h1>
-  {#if jsonData}
-    <JsonView {jsonData} />
-    <button
-      on:click={copyJsonToClipboard}
-      class={showStatus ? "hidden" : ""}
-    >
-      Sign In with
-      <img
-        src="https://assets-global.website-files.com/62535c6262b90afd768b9b26/62536a8f8dc259548c11d1a9_immutable-logo.svg"
-        class="logo"
-        alt="IMX logo"
-      />
-    </button>
-    {#if showStatus}
-      <p class="status">Logged in. Session data in clipboard. Ctrl+V in any text editor.</p>
-    {/if}
-  {/if}
+	<h1 class="title">Base Template with Vite + SvelteKit + Immutable SDK + polyfills</h1>
+	<button on:click={login}>
+		Sign In with
+		<img
+			src="https://assets-global.website-files.com/62535c6262b90afd768b9b26/62536a8f8dc259548c11d1a9_immutable-logo.svg"
+			class="logo"
+			alt="IMX logo"
+		/>
+	</button>
+	{#if showStatus}
+		<p class="status">Logged in. Session data in clipboard. Ctrl+V in any text editor.</p>
+	{/if}
+	{#if jsonData}
+		<button on:click={copyJsonToClipboard} class={showStatus ? 'hidden' : ''}>
+			Copy session token
+			<img
+				src="https://assets-global.website-files.com/62535c6262b90afd768b9b26/62536a8f8dc259548c11d1a9_immutable-logo.svg"
+				class="logo"
+				alt="IMX logo"
+			/>
+		</button>
+		<JsonView {jsonData} />
+	{/if}
 </div>
 
 <style>
-  /* Dark theme styles */
-  :global(body) {
-    background-color: #1c1c1c;
-    color: #ffffff;
-    font-family: 'Poppins', sans-serif;
-  }
-  .dark-theme {
-    background-color: #2c2c2c;
-    border: 1px solid #444444;
-    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
-  }
-  .title {
-    font-size: 1.5rem;
-    margin-bottom: 16px;
-    text-align: center;
-    font-family: 'Montserrat', sans-serif;
-  }
-  
-  /* Existing styles */
-  button {
-    color: #0d0d0d;
-    background-image: linear-gradient(137deg, #f191fa, #83e3f0);
-    min-width: 140px;
-    background-color: #0d0d0d;
-    border: 0 #0d0d0d;
-    border-radius: 48px;
-    margin-top: 16px;
-    padding: 12px 24px;
-    font-weight: 500;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    font-family: 'Montserrat', sans-serif;
-    transition: background-color 0.3s, color 0.3s;
-  }
-  button:hover {
-    background-color: #0d0d0d;
-    color: #ffffff;
-  }
-  pre {
-    color: white;
-    margin-top: 16px;
-    font-family: 'Roboto Mono', monospace;
-  }
-  .container {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    height: 100vh;
-    max-width: 80%;
-    margin: auto;
-  }
-  .logo {
-    max-height: 24pt;
-    margin-left: 8px;
-  }
+	/* Dark theme styles */
+	:global(body) {
+		background-color: #1c1c1c;
+		color: #ffffff;
+		font-family: 'Poppins', sans-serif;
+	}
+	.dark-theme {
+		background-color: #2c2c2c;
+		border: 1px solid #444444;
+		box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+	}
+	.title {
+		font-size: 1.5rem;
+		margin-bottom: 16px;
+		text-align: center;
+		font-family: 'Montserrat', sans-serif;
+	}
+
+	/* Existing styles */
+	button {
+		color: #0d0d0d;
+		background-image: linear-gradient(137deg, #f191fa, #83e3f0);
+		min-width: 140px;
+		background-color: #0d0d0d;
+		border: 0 #0d0d0d;
+		border-radius: 48px;
+		margin-top: 16px;
+		padding: 12px 24px;
+		font-weight: 500;
+		text-decoration: none;
+		display: flex;
+		align-items: center;
+		font-family: 'Montserrat', sans-serif;
+		transition: background-color 0.3s, color 0.3s;
+	}
+	button:hover {
+		background-color: #0d0d0d;
+		color: #ffffff;
+	}
+	pre {
+		color: white;
+		margin-top: 16px;
+		font-family: 'Roboto Mono', monospace;
+	}
+	.container {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+		height: 100vh;
+		max-width: 80%;
+		margin: auto;
+	}
+	.logo {
+		max-height: 24pt;
+		margin-left: 8px;
+	}
 </style>
